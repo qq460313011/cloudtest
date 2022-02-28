63b5e9605ecbc25462961fd93f06c5e9954a455c
liziqi <liziqi@wisdomgarden.com>
2022-02-28 18:47:41 +0800

    payment module base

---
+++ cloud-consumer-order80/src/main/java/com/fw/springcloud/controller/OrderController.java
@@ -0,0 +1,26 @@
+package com.fw.springcloud.controller;
+
+import com.fw.springcloud.entities.CommonResult;
+import com.fw.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@RestController
+public class OrderController {
+
+    public static final String URL = "http://localhost:8001";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/create")
+    public CommonResult<Payment> create(Payment payment) {
+        return restTemplate.postForObject(URL + "/payment/create", payment, CommonResult.class);
+    }
+}
